edccbf8a3e72b972c6915c27ce26bf90abcb5731
Justin Parker <j@gojust.in>
2014-07-03 20:24:40 -0600

    hover effects

--- app/app.jsx
+++ app/app.jsx
@@ -23,40 +23,95 @@ var Page = React.createClass({
 });
 
 var Card = React.createClass({
+  getInitialState: function() {
+    return {hovered: false};
+  },
+  setHovered: function(child) {
+    this.setState({hovered: true});
+  },
+  unsetHovered: function(child) {
+    this.setState({hovered: false});
+  },
   render: function() {
   return (
     <div className='block'>
-      <Title name={this.props.name} />
-      <Attributes arr={this.props.attr} />
-      <CardBg src={this.props.images[0]} num={this.props.num} />
+      <Title name={this.props.name}
+        myHover={this.setHovered}
+        myNoHover={this.unsetHovered}
+        hovered={this.state.hovered} />
+
+      <Attributes arr={this.props.attr}
+        myHover={this.setHovered}
+        myNoHover={this.unsetHovered}
+        hovered={this.state.hovered} />
+
+      <Attributes arr={this.props.links.map(function(e) {
+        return <Link href={e.url} text={e.name} />
+      })} className='links' />
+
+      <CardBg
+        src={this.props.images[0]}
+        num={this.props.num}
+        hovered={this.state.hovered} />
     </div>
   );
   }
 });
 
+var SetHoverMixin = {
+  hover: function(event) { if (this.props.myHover) this.props.myHover(this); },
+  noHover: function(event) { if (this.props.myHover) this.props.myNoHover(this); },
+};
+
 var Title = React.createClass({
+  mixins: [SetHoverMixin],
   render: function() {
-  return (
-    <h1>{this.props.name}</h1>
-  );
+    var style = {
+      'opacity': this.props.hovered ? 0.2 : 0.7
+    };
+    return (
+      <h1 style={style}
+      onMouseEnter={this.hover}
+      onMouseLeave={this.noHover}>
+        {this.props.name}
+      </h1>
+    );
   }
 });
 
 var Attributes = React.createClass({
+  mixins: [SetHoverMixin],
   render: function() {
-  var boxes = this.props.arr.map(function(text) {
-    return <li>{text}</li>;
-  });
-  return (
-    <ul>{boxes}</ul>
-  );
+    var style = {
+      'opacity': this.props.hovered ? 0.2 : 0.7
+    };
+    function generateKey(e) {
+      return (e.props && e.props['text']) || e;
+    }
+    var that = this;
+    var boxes = this.props.arr.map(function(text) {
+      return (
+        <li key={generateKey(text)}
+        onMouseEnter={that.hover}
+        onMouseLeave={that.noHover}>
+          {text}
+        </li>
+      );
+    });
+    return (
+      <ul className={this.props.className}
+      style={style}>
+        {boxes}
+      </ul>
+    );
   }
 });
 
 var CardBg = React.createClass({
   render: function() {
     var style = {
-      'background-image': 'url(' + this.props.src + ')'
+      'background-image': 'url(' + this.props.src + ')',
+      'opacity': this.props.hovered ? 1 : 0.7
     };
     return (
       <div className={'background order-' + this.props.num}
